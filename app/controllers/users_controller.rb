5239276300256cfd4d91a9982fd460917f21be3d
realisticattorney <g.axl.aquila@gmail.com>
2021-01-14 00:53:06 -0300

    point user create action to a single hash, replace form given in the instructions for a form_tag sintax

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,19 @@ class UsersController < ApplicationController
    end
 
    def create
-   end
+    # @user = User.new(user_params)
+    @user = User.new(username: params[:username], email: params[:email], password: params[:password]) #all of this should be in a single hash
+ #form_for does it, pointing to a user field with a hash of values. #manually, simply change the name attribute in the form within view (new.html.erb)
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
+    end
+  end
 
-   
+  private
+
+    def user_params
+      params.require(:user).permit(:username,:email,:password,:authenticity_token)
+    end
 end
