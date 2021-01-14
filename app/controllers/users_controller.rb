a7424ffa7c79c9414af61e98d5d258c79f1a3245
realisticattorney <g.axl.aquila@gmail.com>
2021-01-14 03:51:27 -0300

    add edit action and view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,22 @@
 class UsersController < ApplicationController
    def new
+    @user = User.new
    end
 
    def create
-    # @user = User.new(user_params)
-    @user = User.new(username: params[:username], email: params[:email], password: params[:password]) #all of this should be in a single hash
- #form_for does it, pointing to a user field with a hash of values. #manually, simply change the name attribute in the form within view (new.html.erb)
+    @user = User.new(user_params)
+    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
     if @user.save
       redirect_to new_user_path
     else
       render :new
     end
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+  
 
   private
 
